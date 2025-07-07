91ed0b8756ef75371c17176aae81acf41f411a51
Andrew Ilersich <andrew.ilersich@mail.utoronto.ca>
2025-07-06 20:15:57 -0400

    another tweak to the snail icon

--- icons.js
+++ icons.js
@@ -51,7 +51,7 @@ const exclamation = {
 };
 
 const snail = {
-    path: "M 10 8 L -1 8 A 7 7 0 0 1 -8 1 L -8 -9 L -8 -5 A 2 2 0 0 0 -4 -5 L -4 -9 L -4 -1 A 6 6 0 0 0 2 5 L 4 5 L 2 5 A 6 6 0 0 1 -4 -1 L -4 -1 A 7 7 0 1 1 10 -1 A 6 6 0 0 1 4 5 A 6 3 0 0 1 10 8 M 3 0 A 1 1 0 0 0 4 1 A 2 2 0 0 0 6 -1 A 3 3 0 0 0 3 -4 A 3 3 0 0 0 0 -1 L 0 1 A 4 4 0 0 0 4 5 A 4 4 0 0 1 0 1 L 0 -1 A 3 3 0 0 1 3 -4 A 3 3 0 0 1 6 -1 A 2 2 0 0 1 4 1 A 1 1 0 0 1 3 0",
+    path: "M 10 8 L -1 8 A 7 7 0 0 1 -8 1 L -8 -8 L -8 -5 A 2 2 0 0 0 -4 -5 L -4 -8 L -4 0 A 4 4 0 0 0 0 4 L 3 4 L 0 4 A 4 4 0 0 1 -4 0 L -4 -1 A 7 7 0 0 1 3 -8 A 6 6 0 0 1 9 -2 A 6 6 0 0 1 3 4 A 7 4 0 0 1 10 8 M 3 0 A 2 2 0 0 0 5 -2 A 2 2 0 0 0 3 -4 A 3 3 0 0 0 0 -1 L 0 1 A 3 3 0 0 0 3 4 A 3 3 0 0 1 0 1 L 0 -1 A 3 3 0 0 1 3 -4 A 2 2 0 0 1 5 -2 A 2 2 0 0 1 3 0",
     strokeColor: "rgb(200, 100, 0)",
     strokeWeight: 4,
     strokeOpacity: 1,
@@ -66,4 +66,14 @@ const dash = {
     strokeOpacity: 1,
     fillOpacity: 1,
     scale: 1
+};
+
+const stopwatch = {
+    path: "M 0,0 m -8,0 a 8,8 0 1,0 16,0 a 8,8 0 1,0 -16,0 M 0,0 L 0,-5 M 0,0 L 2,3 M 0,-8 L 0,-11 L -3,-11 L 3,-11 L 0,-11",
+    strokeColor: "rgb(200, 100, 0)",
+    strokeWeight: 4,
+    strokeOpacity: 1,
+    fillColor: "#ffffff",
+    fillOpacity: 1,
+    scale: iconScale
 };
