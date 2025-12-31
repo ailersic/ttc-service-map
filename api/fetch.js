f555074b0fc559e02707243e5a80652e14a7eb7a
Andrew Ilersich <andrew.ilersich@mail.utoronto.ca>
2025-12-30 23:21:08 -0500

    fully migrated to Cloudflare pages from Vercel

--- api/fetch.js
+++
@@ -1,8 +0,0 @@
-import { onRequestGet } from '../functions/api/fetch.js';
-
-export default async function handler(req, res) {
-  const response = await onRequestGet({ request: req });
-  const data = await response.json();
-  
-  res.status(response.status).json(data);
-}
