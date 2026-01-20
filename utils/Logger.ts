74b5c4373f6e80094a5acf8d04796827e61349ab
Tony <antonio.ilersich@gmail.com>
2026-01-20 11:41:29 -0500

    WIP troubleshooting cloudflare logging

--- utils/Logger.ts
+++ utils/Logger.ts
@@ -1,5 +1,5 @@
 export default class Logger {
     static info(...data: any[]) {
-        console.info(`[${new Date().toISOString()}] -`, ...data);
+        console.log(`[${new Date().toISOString()}] -`, ...data);
     }
 };
